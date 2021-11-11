c7a508743eb8925aaf2425be76accd092c2bc744
jiaqishu <91766046+jiaqishu@users.noreply.github.com>
2021-11-11 14:13:35 +0800

    Pasted
    Pass!

---
+++ Assets/Scripts/NumpadScripts/Numpad3.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Numpad3 : MonoBehaviour
+{
+    public LayerMask layer;
+
+    private GameObject itembeinginteracted;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(Camera.main.transform.position, Camera.main.transform.forward, out hit, 2, layer))
+        {
+            itembeinginteracted = hit.collider.gameObject;
+            if (Input.GetMouseButtonUp(0) && this.gameObject == itembeinginteracted)
+            {
+                NumboxController.thenumber += "3";
+            }
+        }
+    }
+}
